eb91a34ca4f0a95a43177d3f149dcf3fb24eed9a
camilocas88 <camilo.castillo@imagemaker.com>
2021-02-16 21:26:57 -0500

    lasts changes

--- js/carousel.js
+++ js/carousel.js
@@ -1,21 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var splide = new Splide('#splide', {
-		type: 'loop',
 		focus: 'center',
 		autoplay: false,
-		// interval: 8000,
-		// flickMaxPages: 1,
 		updateOnMove: true,
-		pagination: false,
+		pagination: true,
 		padding: '0%',
 		perPage: 1,
 		throttle: 300,
-		breakpoints: {
-			// 1440: {
-			// 	perPage: 1,
-			// 	padding: '0%',
-			// },
-		},
 		video: {
 			autoplay: true,
 			mute: true,
@@ -23,10 +14,4 @@ document.addEventListener('DOMContentLoaded', function () {
 			hideControls: false,
 		},
 	}).mount();
-	splide.on('video play', function () {
-		console.log('asdasdasdasdasdas');
-	});
-	splide.on('moved', function () {
-		console.log('Se movio');
-	});
 });
